43350f2d5121e9de7a76c5ea1035c754d7d4d25a
tkskrt23 <tkskrt23@gmail.com>
2022-12-31 01:39:59 +0900

    pet用のルート設定見直しとpets_controllerのメソッド修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :pets
+    has_many :pet_categories, through: :pets     
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :pets
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  # user_idからpet一覧取得用
+  resources :user do
+    resources :pets, only: [:index]
+  end
+  # その他のpet contoroller用
+  resources :pets, only: [:show, :new, :create, :edit, :update, :destroy]
+  
   # テスト用APIのルート設定
   namespace :api do
     namespace :v1 do
       resources :hello, only:[:index]
+      
     end
   end
 end
